605f5df81c07741b8acc2e38488a39bbf20ffd78
Maria Letícia da Silva <marials2607.sl@gmail.com>
2026-06-11 17:19:48 -0300

    Add files via upload

--- Aula04JavaScript/js/script3.js
+++ Aula04JavaScript/js/script3.js
@@ -79,3 +79,162 @@ if(n2 > maior){
     maior = n3
 } 
 alert("O maior número é: "+maior)
+
+/*7. Maior e menor de três números
+Faça um programa que leia três números e mostre o maior e o menor deles.
+*/
+let num1 = Number(prompt("Digite o primeiro número: "))
+let num2 = Number(prompt("Digite o segundo número: "))
+let num3 = Number(prompt("Digite o terceiro número: "))
+
+let maior = num1
+let menor= num1
+
+if(n2>maior){
+    maior = num2
+}
+if(n3>maior){
+    maior = num3
+}
+if(n2<menor){
+    menor = num2
+}
+if(n3<menor){
+    menor = num3
+}
+alert("Maior: "+maior+"\nMenor: "+menor)
+
+/*8. Produto mais barato
+Faça um programa que pergunte o preço de três produtos e informe qual produto deve ser comprado, sabendo que a decisão é sempre pelo mais barato.
+*/
+let p1 = Number(prompt("Digite o preço do produto 1: "))
+let p2 = Number(prompt("Digite o preço do produto 2: "))
+let p3 = Number(prompt("Digite o preço do produto 3: "))
+
+if(p1 < p2 && p1 < p3){
+    alert("Compre o produto 1")
+}else if(p2<p1 && p2<p3){
+    alert("Compre o produto 2")
+}else{
+    alert("Compre o produto 3")
+}
+
+/*9. Ordem decrescente
+Faça um programa que leia três números e mostre-os em ordem decrescente.
+*/
+let a = Number(prompt("Digite o primeiro número: "))
+let b = Number(prompt("Digite o segundo número: "))
+let c = Number(prompt("Digite o terceiro número: "))
+
+if(a>=b && b>=c){
+    alert(a+","+b+","+c)
+}else if(a>=c && c>=b){
+    alert(a+","+c+","+b)
+}else if(b>=a && a>=c){
+    alert(b+","+a+","+c)
+}else if(b>=c && c>=a){
+    alert(b+","+c+","+a)
+}else if(c>=a && a>=b){
+    alert(c+","+a+","+b)
+}else{
+    alert(c+","+b+","+a)
+}
+
+/*10. Turno de estudo
+Faça um programa que pergunte em que turno você estuda.
+
+Digite:
+
+- M → Matutino
+- V → Vespertino
+- N → Noturno
+
+Exiba:
+
+- "Bom dia!"
+- "Boa tarde!"
+- "Boa noite!"
+- "Valor inválido"
+*/
+let turno =prompt("Digite seu turno: M, V ou N: ").toUpperCase()
+
+if(turno =="M"){
+    alert("Bom dia!")
+}else if(turno=="V"){
+    alert("Boa tarde!")
+}else if(turno=="N"){
+    alert("Boa noite!")
+}else{
+    alert("Valor inválido")
+}
+
+/*11. Reajuste salarial
+As organizações CSM resolveram dar um aumento de salário aos seus colaboradores.
+
+Faça um programa que receba o salário de um colaborador e calcule o reajuste conforme a tabela:
+
+| Faixa Salarial | Percentual |
+|---------------|------------|
+| Até R$ 280,00 | 20% |
+| De R$ 280,01 até R$ 700,00 | 15% |
+| De R$ 700,01 até R$ 1.500,00 | 10% |
+| Acima de R$ 1.500,00 | 5% |
+
+Após o reajuste, informe:
+
+- Salário antes do reajuste;
+- Percentual aplicado;
+- Valor do aumento;
+- Novo salário.
+*/
+let salario = Number(prompt("Digite seu salário: "))
+let percentual 
+
+if(salario<=280){
+    percentual= 20
+}else if(salario<=700){
+    percentual= 15
+}else if(salario<=1500){
+    percentual= 10
+}else{
+    percentual= 5
+}
+let aumento = (salario*percentual)/100
+let novoSalario = salario + aumento
+
+alert("Salário antes do reajuste: R$"+salario.toFixed(2)+"\nPercentual aplicado: "+percentual+"%"+"\nValor do aumento: R$"+aumento.toFixed(2)+"\nNovo salário: R$"+novoSalario.toFixed(2))
+
+/*12. Folha de pagamento
+Faça um programa para cálculo de folha de pagamento.
+
+O programa deverá pedir:
+
+- Valor da hora trabalhada;
+- Quantidade de horas trabalhadas no mês.
+
+### Desconto do IR
+
+| Salário Bruto | IR |
+|--------------|----|
+| Até R$ 900,00 | Isento |
+| Até R$ 1.500,00 | 5% |
+| Até R$ 2.500,00 | 10% |
+| Acima de R$ 2.500,00 | 20% |
+
+Considere:
+
+- INSS: 10%
+- Sindicato: 3%
+- FGTS: 11% (não é descontado)
+
+Exemplo de saída:
+
+```text
+Salário Bruto (5 * 220)     : R$ 1100,00
+(-) IR (5%)                : R$   55,00
+(-) INSS (10%)             : R$  110,00
+FGTS (11%)                 : R$  121,00
+Total de descontos         : R$  165,00
+Salário Líquido            : R$  935,00
+*/
+
